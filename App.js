e3682acd7347aa9281e976bebbf47023c4e0eb03
LailaaArman <laila.arman@hotmail.com>
2022-02-04 15:50:49 +0100

    Navbar

--- App.js
+++ App.js
@@ -1,49 +1,57 @@
-import React from 'react';
 import { StyleSheet, Text, View, TextInput} from 'react-native';
-import Button from 'react-bootstrap/Button';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
-export default function App() {
 
+const Tab = createMaterialTopTabNavigator();
+function DiscoverScreen() {
   return (
-    <View style={styles.container}>
-
-      <Text>FoodTinder</Text>
-      <TextInput 
-      style={styles.input}
-      placeholder='Username'/>
-
-      <TextInput 
-      style={styles.input}
-      placeholder='Password'/>
-
-<View>
-<Button variant="primary">Primary</Button>{' '}
-</View>
-
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Discover!</Text>
     </View>
   );
 }
 
+function LikesScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Your Likes!</Text>
+    </View>
+  );
+}
+
+function CookbookScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Cookbook!</Text>
+    </View>
+  );
+}
+
+function AccountScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Account!</Text>
+    </View>
+  );
+}
+export default function App() {
+
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Discover" component={DiscoverScreen} />
+        <Tab.Screen name="Likes" component={LikesScreen} />
+        <Tab.Screen name="Cookbook" component={CookbookScreen} />
+        <Tab.Screen name="Account" component={AccountScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  input: { 
-    borderWidth: 1,
-    borderColor: '#777',
-    padding: 8,
-    margin: 10,
-    width: 200,
-  },
-  buttonsContainer: {
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: '100%',
-    marginVertical: 20,
-  }
+
 });
