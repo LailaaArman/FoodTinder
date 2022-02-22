ae5ae3c6bc09618b0dfcbfd92b8736fabca9d904
Alexander Lundqvist <33635814+KrimReaper@users.noreply.github.com>
2022-02-22 17:57:06 +0100

    Initial commit
    Doesn't work..

--- src/store.js
+++ src/store.js
@@ -9,4 +9,38 @@ const rootReducer = combineReducers({
     preferenceReducer: preferenceReducer
 })
 
-export const store = createStore(rootReducer);
+//export const store = createStore(rootReducer);
+
+
+// convert object to string and store in localStorage
+function saveToLocalStorage(state) {
+  try {
+    const serialisedState = JSON.stringify(state);
+    localStorage.setItem("persistantState", serialisedState);
+  } catch (e) {
+    console.warn(e);
+  }
+}
+
+// load string from localStarage and convert into an Object
+// invalid output must be undefined
+function loadFromLocalStorage() {
+  try {
+    const serialisedState = localStorage.getItem("persistantState");
+    if (serialisedState === null) return undefined;
+    return JSON.parse(serialisedState);
+  } catch (e) {
+    console.warn(e);
+    return undefined;
+  }
+}
+
+// create our store from our rootReducers and use loadFromLocalStorage
+// to overwrite any values that we already have saved
+const store = createStore(rootReducer, loadFromLocalStorage());
+
+// listen for store changes and use saveToLocalStorage to
+// save them to localStorage
+store.subscribe(() => saveToLocalStorage(store.getState()));
+
+export default store;
